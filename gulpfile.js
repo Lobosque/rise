74e465df95a07a7ff7fb0e81ab84ef1cb583ddef
Lucas Lobosque <lucas@freta.la>
2014-06-16 18:09:57 -0300

    added option to automatically move compiled rise to admin

--- gulpfile.js
+++ gulpfile.js
@@ -14,9 +14,20 @@ gulp.task('build', function() {
   .pipe(gulp.dest('./site/js/lib'));
 });
 
+gulp.task('copyToAdmin', function() {
+  return gulp.src('./site/js/lib/rise.js')
+  .pipe(concat('rise.js'))
+  .pipe(gulp.dest('../admin/site/js/vendor'));
+});
+
 // Rerun the task when a file changes
 gulp.task('watch', function() {
   gulp.watch(buildPaths, ['build']);
 });
 
+gulp.task('watchCp', function() {
+  gulp.watch(buildPaths, ['build', 'copyToAdmin']);
+});
+
 gulp.task('default', ['build', 'watch']);
+gulp.task('cp', ['build', 'copyToAdmin', 'watchCp']);
